721e655d5769b8ee201203481f89d75d0b37898f
Gravita <12893402+gravit0@users.noreply.github.com>
2025-09-19 21:24:07 +0700

    Add support launcher 5.7.x, usage new frontend and backend

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -e
 set -e
-BACKEND_BRANCH=main
 FRONTEND_BRANCH=main
-MODULE_BRANCH=main
-LAUNCHSERVER_DOCKER_BRANCH=main
+MODULE_BRANCH=support/5.7.x
+
 SIMPLECABINET_REMOTE=localhost:17549
 SIMPLECABINET_PROTOCOL=http
 
@@ -11,62 +10,39 @@ SIMPLECABINET_REMOTE_URL=$SIMPLECABINET_PROTOCOL:\\/\\/$SIMPLECABINET_REMOTE
 
 echo -e "\033[32mPhase 0: \033[33mDownload repositories\033[m";
 
-if ! [ -d backend-src ]; then
-    git clone --depth=1 -b $BACKEND_BRANCH https://github.com/SimpleCabinet/SimpleCabinetWebAPI.git backend-src
-fi
-
 if ! [ -d frontend-src ]; then
-    git clone --depth=1 -b $FRONTEND_BRANCH https://github.com/SimpleCabinet/SimpleCabinetFrontend2.git frontend-src
+    git clone --depth=1 -b $FRONTEND_BRANCH https://github.com/SimpleCabinet/SimpleCabinetFrontend.git frontend-src
 fi
 
 if ! [ -d module-src ]; then
     git clone --depth=1 -b $MODULE_BRANCH https://github.com/SimpleCabinet/SimpleCabinetModule.git module-src
 fi
 
-if ! [ -d dockered-src ]; then
-    git clone --depth=1 -b $LAUNCHSERVER_DOCKER_BRANCH https://github.com/GravitLauncher/LauncherDockered.git dockered-src
-    ln -s dockered-src/Dockerfile.launcher Dockerfile.launcher
-    ln -s dockered-src/setup-docker.sh setup-docker.sh
-fi
-
 echo -e "\033[32mPhase 1: \033[33mBuild SimpleCabinet\033[m";
 
-docker run --rm -v "$(pwd)/backend-src:/app" -w /app -u $(id -u):$(id -g) eclipse-temurin:21-noble ./gradlew assemble
 docker run --rm -v "$(pwd)/module-src:/app" -w /app -u $(id -u):$(id -g) eclipse-temurin:21-noble ./gradlew build
-cp frontend-src/.env.example frontend-src/.env
-sed -i "s/URL=http:\/\/localhost:8080\//URL=$SIMPLECABINET_REMOTE_URL\/api\//" frontend-src/.env
-mkdir backend-configuration
-cp backend-src/src/main/resources/application.properties backend-configuration/application.properties
-cp -r backend-src/src/main/resources/templates backend-configuration
-sed -i "s/spring.datasource.url=jdbc:postgresql:\/\/localhost:5432\/simplecabinet/spring.datasource.url=jdbc:postgresql:\/\/postgres:5432\/simplecabinet/" backend-configuration/application.properties
-sed -i "s/storage.file.remoteUrl=http:\/\/localhost:8080\/assets\//storage.file.remoteUrl=$SIMPLECABINET_REMOTE_URL\/userassets\//" backend-configuration/application.properties
-
-echo -e "\033[32mPhase 1.1: \033[33mGenerate key pair\033[m";
-
-openssl ecparam -name prime256v1 -genkey -noout -out private_key.pem
-openssl ec -in private_key.pem -pubout -out public_key.pem
-openssl pkcs8 -topk8 -nocrypt -in private_key.pem -outform DER -out backend-configuration/ecdsa
-openssl ec -in private_key.pem -pubout -outform DER -out backend-configuration/ecdsa.pub
 
 echo -e "\033[32mPhase 2: \033[33mBuild Docker containers\033[m";
 
 sed -i "s/LAUNCHSERVER_ADDRESS_PLACEHOLDER/$SIMPLECABINET_REMOTE\/launcher/" docker-compose.yml || true
+sed -i "s/SIMPLECABINET_REMOTE_URL_PLACEHOLDER/$SIMPLECABINET_REMOTE_URL\/userassets\//" docker-compose.yml || true
 docker compose up -d --build
 
-echo -e "\033[32mPhase 3: \033[33mSleep 40 seconds\033[m";
+echo -e "\033[32mPhase 3: \033[33mSleep 60 seconds\033[m";
 
-sleep 40
+sleep 60
 
 echo -e "\033[32mPhase 3.1: \033[33mRun basic initialization\033[m";
 
 docker compose exec simplecabinet curl http://simplecabinet:8080/setup > setup.json
 ADMIN_API_TOKEN=$(cat setup.json | jq ".accessToken")
-docker compose cp module-src/build/libs/*.jar gravitlauncher:/app/data/modules/SimpleCabinet_module.jar
+docker compose cp module-src/build/libs/*.jar gravitlauncher:/app/data/SimpleCabinet_module.jar
+echo "modules load SimpleCabinet_module.jar" | docker compose exec -T gravitlauncher socat UNIX-CONNECT:control-file -
 docker compose restart gravitlauncher
 
-echo -e "\033[32mPhase 3.2: \033[33mSleep 10 seconds\033[m";
+echo -e "\033[32mPhase 3.2: \033[33mSleep 20 seconds\033[m";
 
-sleep 10
+sleep 20
 
 echo -e "\033[32mPhase 3.3: \033[33mRun 'simplecabinet install' command\033[m";
 
